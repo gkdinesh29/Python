99026b4ffa44dd9286d8e8ac7df0d2899ae40485
gkdinesh29 <gkdinesh.dev@gmail.com>
2021-01-24 00:48:17 -0500

    script to handles full response

---
+++ fullresponse.py
@@ -0,0 +1,16 @@
+import requests
+import hashlib
+apiUrl = "https://www.docker.com/"
+filename = "fullresponse.txt"
+chunk_size = 100
+output ="fullcheck.txt"
+response = requests.get( apiUrl )
+
+with open(filename, 'wb') as fd:
+    for chunk in response.iter_content(chunk_size):
+        fd.write(chunk)
+m = hashlib.md5()
+print(m)
+print(m.hexdigest())
+with open(output,'wb') as fd:
+    fd.write(m.hexdigest())
