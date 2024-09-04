41ccb103ffe722f2c5fa861795e5c193af3379b1
gkdinesh29 <53544310+gkdinesh29@users.noreply.github.com>
2024-09-04 14:08:45 +0530

    Updated UPPER_CASE naming style

--- fullresponse.py
+++ fullresponse.py
@@ -1,29 +1,27 @@
 import requests
 import hashlib
 
-apiUrl = "https://www.jenkins.io/"
-filename = "fullresponse.txt"
-chunk_size = 100
-output = "fullcheck.txt"
+API_URL = "https://www.jenkins.io/"
+FILENAME = "fullresponse.txt"
+CHUNK_SIZE = 100
+OUTPUT = "fullcheck.txt"
 
 # Download content from the API URL and save it to a file
-response = requests.get(apiUrl)
+response = requests.get(API_URL)
 
-with open(filename, 'wb') as fd:
-    for chunk in response.iter_content(chunk_size):
+with open(FILENAME, 'wb') as fd:
+    for chunk in response.iter_content(CHUNK_SIZE):
         fd.write(chunk)
 
 # Compute the MD5 hash of the downloaded file
 m = hashlib.md5()
-with open(filename, 'rb') as fd:
-    for chunk in iter(lambda: fd.read(chunk_size), b""):
+with open(FILENAME, 'rb') as fd:
+    for chunk in iter(lambda: fd.read(CHUNK_SIZE), b""):
         m.update(chunk)
 
 # Print the MD5 hash
 print("MD5 Hash:", m.hexdigest())
 
 # Write the MD5 hash to an output file
-with open(output, 'w') as fd:
+with open(OUTPUT, 'w') as fd:
     fd.write(m.hexdigest())
-
-    
