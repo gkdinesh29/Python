9df18b50ff6bb9d71ac57309da075998959fbd97
gkdinesh29 <53544310+gkdinesh29@users.noreply.github.com>
2024-09-04 14:05:19 +0530

    Update fullresponse.py

--- fullresponse.py
+++ fullresponse.py
@@ -1,17 +1,29 @@
 import requests
 import hashlib
+
 apiUrl = "https://www.jenkins.io/"
 filename = "fullresponse.txt"
 chunk_size = 100
-output ="fullcheck.txt"
-response = requests.get( apiUrl )
+output = "fullcheck.txt"
+
+# Download content from the API URL and save it to a file
+response = requests.get(apiUrl)
 
 with open(filename, 'wb') as fd:
     for chunk in response.iter_content(chunk_size):
         fd.write(chunk)
+
+# Compute the MD5 hash of the downloaded file
 m = hashlib.md5()
-print(m)
-print(m.hexdigest())
-with open(output,'wb') as fd:
+with open(filename, 'rb') as fd:
+    for chunk in iter(lambda: fd.read(chunk_size), b""):
+        m.update(chunk)
+
+# Print the MD5 hash
+print("MD5 Hash:", m.hexdigest())
+
+# Write the MD5 hash to an output file
+with open(output, 'w') as fd:
     fd.write(m.hexdigest())
+
     
